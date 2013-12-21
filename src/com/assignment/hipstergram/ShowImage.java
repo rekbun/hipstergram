902aec168b5d93c91358de3639b345004c1cf41e
rakesh <rkchoudhary0208@gmail.com>
2013-12-21 05:44:20 +0530

    added new screen

--- src/com/assignment/hipstergram/ShowImage.java
+++ src/com/assignment/hipstergram/ShowImage.java
@@ -1,6 +1,7 @@
 package com.assignment.hipstergram;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -9,9 +10,11 @@ public class ShowImage extends Activity {
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
+        Intent intent=getIntent();
+
         setContentView(R.layout.showimage);
         ImageView view=(ImageView)findViewById(R.id.imageView);
-        view.setImageURI(Uri.parse(savedInstance.getString("path")));
+        view.setImageURI(Uri.parse(intent.getStringExtra("path")));
     }
 
 }
